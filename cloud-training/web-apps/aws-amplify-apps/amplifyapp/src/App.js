a7b4bf7c580b3b37bfed8e20226c0b8a3e1a818f
DIEGODAVILA <diego.davila@pucp.edu.pe>
2022-02-05 11:38:47 -0500

    delete div tag

--- cloud-training/web-apps/aws-amplify-apps/amplifyapp/src/App.js
+++ cloud-training/web-apps/aws-amplify-apps/amplifyapp/src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
       </header>
-    </div>
+
   );
+
 }
 
 export default App;
